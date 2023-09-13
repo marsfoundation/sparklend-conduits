9dc46b7d162760910d77fb63793d6417a538641f
lucas-manuel <lucasmanuel.tech@gmail.com>
2023-09-13 14:27:00 +0800

    fix: rm file

--- test/Interfaces.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity ^0.8.13;
-
-import { IERC20 } from "erc20-helpers/interfaces/IERC20.sol";
-
-interface IAToken is IERC20 {
-    function scaledBalanceOf(address user) external view returns (uint256);
-}
