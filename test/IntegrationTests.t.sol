3c95e826f222296bea5b5315d279a7aba8187bd7
lucas-manuel <lucasmanuel.tech@gmail.com>
2023-10-03 10:30:21 +0700

    feat: add e2e type testing with logging

--- test/IntegrationTests.t.sol
+++ test/IntegrationTests.t.sol
@@ -3,8 +3,18 @@ pragma solidity ^0.8.13;
 
 import "dss-test/DssTest.sol";
 
-import { IPool }   from "aave-v3-core/contracts/interfaces/IPool.sol";
-import { IAToken } from "aave-v3-core/contracts/interfaces/IAToken.sol";
+// Debug
+import { AaveProtocolDataProvider } from "aave-v3-core/contracts/misc/AaveProtocolDataProvider.sol";
+import { BorrowLogic }              from "aave-v3-core/contracts/protocol/libraries/logic/BorrowLogic.sol";
+import { SupplyLogic }              from "aave-v3-core/contracts/protocol/libraries/logic/SupplyLogic.sol";
+// Debug
+
+import { IAToken }           from "aave-v3-core/contracts/interfaces/IAToken.sol";
+import { IPoolConfigurator } from "aave-v3-core/contracts/interfaces/IPoolConfigurator.sol";
+import { IPool }             from "aave-v3-core/contracts/interfaces/IPool.sol";
+
+import { IVariableDebtToken as IDToken }
+    from "aave-v3-core/contracts/interfaces/IVariableDebtToken.sol";
 
 import { AllocatorRegistry } from "dss-allocator/AllocatorRegistry.sol";
 import { AllocatorRoles }    from "dss-allocator/AllocatorRoles.sol";
@@ -13,14 +23,26 @@ import { IERC20 } from "erc20-helpers/interfaces/IERC20.sol";
 
 import { UpgradeableProxy } from "upgradeable-proxy/UpgradeableProxy.sol";
 
-import { SparkConduit, IInterestRateDataSource } from 'src/SparkConduit.sol';
+import {  IInterestRateDataSource, PotLike, SparkConduit } from 'src/SparkConduit.sol';
+
+import { DaiInterestRateStrategy, DataTypes } from 'src/DaiInterestRateStrategy.sol';
 
 contract ConduitIntegrationTestBase is DssTest {
 
+    // Debug
+    AaveProtocolDataProvider data
+        = AaveProtocolDataProvider(0xFc21d6d146E6086B8359705C8b28512a983db0cb);
+
     address DAI  = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address ADAI = 0x4DEDf26112B3Ec8eC46e7E31EA5e123490B05B8B;
+    address DDAI = 0xf705d2B7e92B3F38e6ae7afaDAA2fEE110fE5914;
     address POOL = 0xC13e21B648A5Ee794902342038FF3aDAB66BE987;
     address POT  = 0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
+
+    address POOL_CONFIGURATOR = 0x542DBa469bdE58FAeE189ffB60C6b49CE60E0738;
+
+    uint256 constant RBPS = RAY / 10_000;
 
     address admin     = makeAddr("admin");
     address buffer1   = makeAddr("buffer1");
@@ -35,11 +57,17 @@ contract ConduitIntegrationTestBase is DssTest {
     uint256 ADAI_SUPPLY;         // aToken.totalSupply()
     uint256 ADAI_SCALED_SUPPLY;  // aToken.totalSupply()
     uint256 INDEX;               // pool.getReserveNormalizedIncome(DAI)
+    uint256 START;               // block.timestamp
+    uint256 DSR_APR;             // Annualized pot.dsr()
+    uint256 DSR;                 // pot.dsr()
 
-    AllocatorRegistry registry;
-    AllocatorRoles    roles;
+    AllocatorRegistry       registry;
+    AllocatorRoles          roles;
+    DaiInterestRateStrategy interestStrategy;
 
     IERC20  dai    = IERC20(DAI);
+    IERC20  weth   = IERC20(WETH);
+    IERC20  dToken = IERC20(DDAI);
     IAToken aToken = IAToken(ADAI);
     IPool   pool   = IPool(POOL);
 
@@ -48,11 +76,27 @@ contract ConduitIntegrationTestBase is DssTest {
     function setUp() public virtual {
         vm.createSelectFork(getChain('mainnet').rpcUrl, 18_090_400);
 
+        // Debug
+
+        address borrowLogic = deployCode("BorrowLogic.sol:BorrowLogic", bytes(""));
+        address supplyLogic = deployCode("SupplyLogic.sol:SupplyLogic", bytes(""));
+
+        address deployedBorrowLogic = 0x5d834EAD0a80CF3b88c06FeeD6e8E0Fcae2daEE5;
+        address deployedSupplyLogic = 0x39dF4b1329D41A9AE20e17BeFf39aAbd2f049128;
+
+        vm.etch(deployedBorrowLogic, borrowLogic.code);
+        vm.etch(deployedSupplyLogic, supplyLogic.code);
+
+        // Debug
+
         // Starting state at block 18_090_400
         LIQUIDITY          = 3042.894995046294009693 ether;
         ADAI_SUPPLY        = 200_668_890.552846452355198767 ether;
         ADAI_SCALED_SUPPLY = 199_358_171.788361925857232792 ether;
         INDEX              = 1.006574692939479711169088718e27;
+        START              = 1_694_160_383;
+        DSR_APR            = 0.048790164207174267760128000e27;
+        DSR                = 1.000000001547125957863212448e27;
 
         UpgradeableProxy proxy = new UpgradeableProxy();
         SparkConduit     impl  = new SparkConduit(POOL, POT);
@@ -63,6 +107,16 @@ contract ConduitIntegrationTestBase is DssTest {
 
         registry = new AllocatorRegistry();
         roles    = new AllocatorRoles();
+
+        interestStrategy = new DaiInterestRateStrategy(
+            address(dai),
+            conduit,
+            30 * RBPS,
+            75_00 * RBPS
+        );
+
+        vm.prank(POOL_CONFIGURATOR);
+        pool.setReserveInterestRateStrategyAddress(DAI, address(interestStrategy));
 
         conduit.setRoles(address(roles));
         conduit.setRegistry(address(registry));
@@ -81,6 +135,7 @@ contract ConduitIntegrationTestBase is DssTest {
         IERC20(DAI).approve(address(conduit), type(uint256).max);
 
         conduit.setAssetEnabled(DAI, true);
+        conduit.setSubsidySpread(50 * RBPS);
     }
 
     function _setupOperatorRole(bytes32 ilk_, address operator_) internal {
@@ -248,24 +303,37 @@ contract ConduitDepositIntegrationTests is ConduitIntegrationTestBase {
             totalShares: expectedShares
         });
 
-        vm.warp(block.timestamp + 1 days);
+        vm.warp(block.timestamp + 365 days);
 
-        uint256 newIndex = pool.getReserveNormalizedIncome(DAI);
+        uint256 expectedValue1 = 100 ether;
 
-        // +1 for rounding
-        uint256 expectedValue  = expectedScaledBalance * newIndex / 1e27 + 1;
-        uint256 expectedSupply = (ADAI_SUPPLY + 100 ether) * 1e27 / INDEX * newIndex / 1e27 + 1;
+        for (uint256 i; i < 86400 * 365; ++i) {
+            expectedValue1 = expectedValue1 * DSR / 1e27;
+        }
 
-        // Show interest accrual
-        assertEq(expectedValue, 100.013366958918209602 ether);
+        uint256 expectedValue2 = 100 ether + 100 ether * DSR_APR / 1e27;
 
-        _assertInvariants();
+        // uint256 newIndex = pool.getReserveNormalizedIncome(DAI);
+
+        // // +1 for rounding
+        // uint256 expectedValue  = expectedScaledBalance * newIndex / 1e27 + 1;
+        // uint256 expectedSupply = (ADAI_SUPPLY + 100 ether) * 1e27 / INDEX * newIndex / 1e27 + 1;
+
+        // // Show interest accrual
+        // assertEq(expectedValue, 100.013366958918209602 ether);
+
+        assertEq(expectedValue1, 1);
+        assertEq(expectedValue2, 2);
+
+        assertEq(expectedValue1 - expectedValue2, 6);
+
+        // _assertInvariants();
 
         _assertATokenState({
             scaledBalance:     expectedScaledBalance,
             scaledTotalSupply: ADAI_SCALED_SUPPLY + expectedScaledBalance,
-            balance:           expectedValue,
-            totalSupply:       expectedSupply
+            balance:           3,
+            totalSupply:       4
         });
     }
 
@@ -359,8 +427,304 @@ contract ConduitDepositIntegrationTests is ConduitIntegrationTestBase {
             ilk2Shares:  expectedIlk2Shares,
             totalShares: expectedIlk1Shares + expectedIlk2Shares
         });
+
+        // TODO: Warp time and show interest accrual
     }
 
+}
+
+contract ConduitRequestFundsE2ETests is ConduitIntegrationTestBase {
+
+    // TODO: Update
+
+    address borrower1 = makeAddr("borrower1");
+    address borrower2 = makeAddr("borrower2");
+
+    function test_requestFunds_e2e_scenario1() external {
+        deal(DAI,  buffer1,   10_000_000 ether);
+        deal(DAI,  buffer2,   10_000_000 ether);
+        deal(WETH, borrower1, 10_000_000 ether);
+        deal(WETH, borrower2, 10_000_000 ether);
+
+        vm.warp(START + 1 days);
+
+        /*************************************************/
+        /*** 1. Recompute To Update Timestamp for Logs ***/
+        /*************************************************/
+
+        interestStrategy.recompute();  // Recompute to get timestamps set up
+
+        /***************************************************************************/
+        /*** 2. Clear out all DAI balance in aDAI to have a clean starting state ***/
+        /***************************************************************************/
+
+        vm.startPrank(borrower1);
+
+        weth.approve(address(pool), 10_000_000 ether);
+        pool.supply(WETH, 10_000_000 ether, borrower1, 0);
+        pool.borrow(DAI, dai.balanceOf(ADAI), 2, 0, borrower1);
+
+        vm.warp(block.timestamp + 1 days);
+
+        vm.stopPrank();
+
+        /*****************************/
+        /*** 3. Log Starting State ***/
+        /*****************************/
+
+        _logAll("3. Log Starting State");
+
+        _logConstantRateState();
+
+        /******************************************************************************/
+        /*** 4. Deposit 10m DAI from SparkDAO using the allocation system + conduit ***/
+        /******************************************************************************/
+
+        vm.prank(operator1);
+        conduit.deposit(ILK1, DAI, 10_000_000 ether);
+
+        interestStrategy.recompute();
+
+        _logAll("4. Deposit 10m DAI from ilk1 using the allocation system + conduit, also recompute to get correct debt ratio");
+
+        /***********************************************************************************/
+        /*** 5. Borrow the full 10m DAI as an external actor (borrower1) using SparkLend ***/
+        /***********************************************************************************/
+
+        vm.startPrank(borrower2);
+
+        weth.approve(address(pool), 10_000_000 ether);
+        pool.supply(WETH, 10_000_000 ether, borrower2, 0);
+        pool.borrow(DAI, 10_000_000 ether, 2, 0, borrower2);
+
+        vm.stopPrank();
+
+        _logAll("5. Borrow the full 10m DAI as an external actor (borrower1) using SparkLend");
+
+        /**********************************************************************************/
+        /*** 6. Request 10m DAI back as the SubDAO, demonstrate no rate change in Spark ***/
+        /**********************************************************************************/
+
+        vm.prank(operator1);
+        conduit.requestFunds(ILK1, DAI, 10_000_000 ether);
+
+        _logAll("6. Request 10m DAI back as the SubDAO, demonstrate no rate change in Spark");
+
+        /******************************************************************************/
+        /*** 7. Recompute in interest strategy, demonstrate no rate change in Spark ***/
+        /******************************************************************************/
+
+        interestStrategy.recompute();
+
+        _logAll("7. Recompute in interest strategy, demonstrate no rate change in Spark");
+
+        /**************************************************************/
+        /*** 8. Borrow again as borrower2, demonstrate state update ***/
+        /**************************************************************/
+
+        vm.warp(block.timestamp + 365 days);
+
+        deal(DAI, ADAI, 1 ether);
+        vm.prank(borrower1);
+        pool.borrow(DAI, 1 ether, 2, 0, borrower1);
+
+        _logAll("8. Borrow again as borrower2, demonstrate state update");
+
+        /*******************************************************************/
+        /*** 9. Repay 1/3 debt as borrower1, demo interest rate decrease ***/
+        /*******************************************************************/
+
+        uint256 debtOwed = dToken.balanceOf(address(borrower2));
+
+        deal(DAI, borrower2, debtOwed);
+
+        vm.startPrank(borrower2);
+        dai.approve(address(pool), debtOwed / 3);
+        pool.repay(DAI, debtOwed / 3, 2, borrower2);
+        vm.stopPrank();
+
+        _logAll("9. Repay 1/3 debt as borrower1, demo interest rate decrease");
+
+        /***************************************************************************/
+        /*** 10. Repay another 1/3 debt as borrower1, demo interest rate decrease ***/
+        /***************************************************************************/
+
+        vm.startPrank(borrower2);
+        dai.approve(address(pool), debtOwed / 3);
+        pool.repay(DAI, debtOwed / 3, 2, borrower2);
+        vm.stopPrank();
+
+        _logAll("10. Repay another 1/3 debt as borrower1, demo interest rate decrease");
+
+        /**********************************************************************************/
+        /*** 11. Repay all debt as borrower1, demo interest rate return to DSR + spread ***/
+        /**********************************************************************************/
+
+        vm.startPrank(borrower2);
+        dai.approve(address(pool), debtOwed / 3);
+        pool.repay(DAI, debtOwed / 3, 2, borrower2);
+        vm.stopPrank();
+
+        _logAll("11. Repay all debt as borrower1, demo interest rate return to DSR + spread");
+
+        /****************************************************************/
+        /*** 12. Withdraw 10m DAI + interest from conduit as SparkDAO ***/
+        /****************************************************************/
+
+        vm.prank(operator1);
+        conduit.withdraw(ILK1, DAI, type(uint256).max);
+
+        _logAll("12. Withdraw 10m DAI + interest from conduit as SparkDAO");
+    }
+
+    function _logExchangeRates() internal view {
+        uint256 income = pool.getReserveNormalizedIncome(DAI);
+        uint256 debt   = pool.getReserveNormalizedVariableDebt(DAI);
+
+        _log("borrower exchangeRate", debt,   27);
+        _log("lender exchangeRate  ", income, 27);
+    }
+
+    function _logPositionState() internal {
+        console.log("----");
+        console.log("Position State");
+        console.log("----");
+
+        _log("conduit shares     ", conduit.shares(DAI, ILK1));
+        _log("conduit position   ", conduit.getDeposits(DAI, ILK1));
+        _log("conduit aToken     ", aToken.balanceOf(address(conduit)));
+        _log("borrower dToken    ", dToken.balanceOf(address(borrower2)));
+        _log("borrower DAI       ", dai.balanceOf(address(borrower2)));
+        _log("buffer DAI         ", dai.balanceOf(address(buffer1)));
+        _log("Spark DAI liquidity", dai.balanceOf(ADAI));
+    }
+
+    function _logDebtInfoState() internal view {
+        IInterestRateDataSource.InterestData memory conduitData = conduit.getInterestData(DAI);
+
+        console.log("----");
+        console.log("Conduit Rate State");
+        console.log("----");
+
+        _log("currentDebt", conduitData.currentDebt, 18);
+        _log("targetDebt ", conduitData.targetDebt,  18);
+
+        console.log("lastUpdate     ", (interestStrategy.getLastUpdateTimestamp() - START) / 84600);
+    }
+
+    function _logConstantRateState() internal view {
+        console.log("----");
+        console.log("Constant Conduit/Strategy Rate State");
+        console.log("----");
+
+        IInterestRateDataSource.InterestData memory conduitData = conduit.getInterestData(DAI);
+
+        _log("baseRate      ", conduitData.baseRate,    27);
+        _log("subsidyRate   ", conduitData.subsidyRate, 27);
+        _log("spread        ", interestStrategy.spread(), 27);
+        _log("baseBorrowRate", interestStrategy.getBaseBorrowRate(), 27);
+    }
+
+    function _logSparkRateState() internal {
+        (
+            ,
+            ,
+            ,
+            ,
+            ,
+            uint256 liquidityRate,
+            uint256 variableBorrowRate,
+            ,
+            ,
+            uint256 liquidityIndex,
+            uint256 variableBorrowIndex,
+            uint256 lastUpdateTimestamp
+        ) = data.getReserveData(DAI);
+
+        uint256 income = pool.getReserveNormalizedIncome(DAI);
+        uint256 debt   = pool.getReserveNormalizedVariableDebt(DAI);
+
+        console.log("----");
+        console.log("Spark Reserve State");
+        console.log("----");
+
+        _log("liquidityRate        ", liquidityRate,       27);
+        _log("variableBorrowRate   ", variableBorrowRate,  27);
+        _log("borrower exchangeRate", debt,                27);
+        _log("variableBorrowIndex  ", variableBorrowIndex, 27);
+        _log("liquidityIndex       ", liquidityIndex,      27);
+        _log("lender exchangeRate  ", income,              27);
+
+        console.log("lastUpdate           ", (lastUpdateTimestamp - START) / 86400);
+
+        vm.warp(block.timestamp + 1 days);
+
+        console.log("----");
+        console.log("Exchange Rates after 1 day");
+        console.log("----");
+
+        _logExchangeRates();
+    }
+
+    function _logAll(string memory label) internal {
+        console.log("");
+        console.log("");
+        console.log("");
+        console.log("----");
+        console.log(label);
+        console.log("   Day:", (block.timestamp - START) / 86400);
+        console.log("----");
+
+        _logPositionState();
+        _logDebtInfoState();
+        _logSparkRateState();
+    }
+
+    function _log(string memory key, uint256 value, uint256 decimals)
+        internal view
+    {
+        uint256 whole = value / 10 ** decimals;
+        uint256 part;
+
+        if (decimals > 4) {
+            part = (value % 10 ** decimals) / 10 ** (decimals - 4);
+        } else {
+            part = (value % 10 ** decimals) * 10 ** (4 - decimals);
+        }
+
+        string memory wholeString = vm.toString(whole);
+        string memory formattedWholeString = "";
+        uint256 length = bytes(wholeString).length;
+
+        for (uint256 i = 0; i < length; i++) {
+            if (i > 0 && (length - i) % 3 == 0) {
+                formattedWholeString = string(abi.encodePacked(formattedWholeString, ","));
+            }
+            bytes1 char = bytes(wholeString)[i];
+            formattedWholeString
+                = string(abi.encodePacked(formattedWholeString, string(abi.encodePacked(char))));
+        }
+
+        string memory partString = vm.toString(part);
+
+        uint256 leadingZeros = 4 - bytes(partString).length;
+
+        string memory zeros = "";
+        for (uint256 i = 0; i < leadingZeros; i++) {
+            zeros = string(abi.encodePacked(zeros, "0"));
+        }
+
+        console.log(key, string(abi.encodePacked(
+            formattedWholeString,
+            ".",
+            zeros,
+            partString
+        )));
+    }
+
+    function _log(string memory key, uint256 value) internal view {
+        _log(key, value, 18);
+    }
 }
 
 contract ConduitWithdrawIntegrationTests is ConduitIntegrationTestBase {
